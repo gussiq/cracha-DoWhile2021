c8187d824e9965496d15b86c64b625f3b0c1688a
Gustavo Siqueira <92948315+gussiq@users.noreply.github.com>
2021-10-22 22:42:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const LinksSocialMedia = {
+  github: 'gussiq',
+  youtube: 'maykbrito',
+  facebook: 'gustavo.siqueira.752',
+  instagram: 'gusiqueiiraa',
+  twitter: 'sicgusta'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+
+      UserImage.src = data.avatar_url
+    })
+}
+
+getGitHubProfileInfos()
